8bf4013cdf56f94ed08b134f5daf3c71444b17f0
Gilmar <gilmar.odp@gmail.com>
2020-04-24 14:12:44 -0300

    Criando sistema de rotas simples para chamar as rotas com funçoes

--- app/config/Router.php
+++ app/config/Router.php
@@ -1,2 +1,15 @@
 <?php
 
+$this->get('/', function () {
+    echo "Estou na home! :D";
+});
+
+$this->get('/home', function () {
+    echo "Estou na home! :D";
+});
+
+$this->get('/about/teste', function () {
+    echo "Estou na about teste! :D";
+});
+
+$this->get('/categoria', 'MyController@method');
